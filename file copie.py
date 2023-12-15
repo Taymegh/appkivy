2a070c199e74f5fbcf60e0572ad3a48f15fdd82b
Taymegh <tmeghiche@gmail.com>
2023-12-15 23:58:30 +0100

    transfer build app test

--- file copie.py
+++ file copie.py
@@ -11,35 +11,53 @@ from kivy.core.text import LabelBase
 class PundizzApp(App):
     def build(self):
 
+
         LabelBase.register(name="Superfont", fn_regular='superfont.ttf')
     
         label_layout= RelativeLayout(size=(Window.width, Window.height))
 
         Window.clearcolor= (0.9, 0.8, 0.1)
         myLabel= Label(
-            text="[b]Pundi's[/b]", markup=True, font_name='Superfont', font_size=50, pos_hint= {'center_x':0.5,'center_y': 0.9})
+            text="[b]Pundi's[/b]", 
+            markup=True, 
+            font_name='Superfont', 
+            font_size=50, 
+            pos_hint= {'center_x': 1.125,'center_y': 0.9})
         myLabel.color = (0.5, 0.0, 0.2)
 
         label_layout.add_widget(myLabel)
 
         image_layout= RelativeLayout(size=(Window.width, Window.height))
-        monImage = Image(
+        self.monImage = Image(
             source="image2.jpg",
-            size_hint=(None, None), 
             size=(200, 200), 
-            pos_hint= {'center_x': 0.5, 'center_y': 0.5})
-        # myButton = Button(text= "Jouer", size= (40, 40), size_hint=(0.5, 0.1), pos_hint= {'center_x':0.5,'center_y': 0.2})
+            size_hint=(None, None), 
+            pos_hint= {'center_x': 0.1, 'center_y': 0.7})
+        
 
-        image_layout.add_widget(monImage)
+        button_layout= RelativeLayout(size=(Window.width, Window.height))
+        myButton = Button(
+            text= "Jouer", 
+            size= (150, 100), 
+            size_hint=(None, None),  
+            pos_hint= {'center_x': -0.9,'center_y': 0.5})
+        button_layout.add_widget(myButton)
+        myButton.bind(on_press=self.hide_image)
+        image_layout.add_widget(self.monImage)
 
         fenetre= BoxLayout(size=(Window.width, Window.height))
         fenetre.add_widget(label_layout)
         fenetre.add_widget(image_layout)
-        # fenetre.add_widget(myButton)
+        fenetre.add_widget(button_layout)
 
 
         return fenetre
     
+    def hide_image(self, instance):
+        if self.monImage.opacity == 0:
+            self.monImage.opacity= 1
+        elif self.monImage.opacity == 1:
+            self.monImage.opacity= 0
 
 
 PundizzApp().run()
