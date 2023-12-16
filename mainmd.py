3cb0595af30e932f82593382ae3974ae415e4625
Taymegh <tmeghiche@gmail.com>
2023-12-16 13:12:02 +0100

    mainmd small changes

--- mainmd.py
+++ mainmd.py
@@ -1,22 +1,35 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.selectioncontrol import MDSwitch
+from kivymd.uix.label import MDLabel
+from kivy.uix.actionbar import ActionBar
+from kivy.uix.boxlayout import BoxLayout
+
 # from kivymd.uix.label import MDLabel
 
 
 class mainApp(MDApp):
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Red"
-
-        return (
-            MDScreen(
-                MDRectangleFlatButton(
-                    text= "Bonjour, Taysir.",
-                    pos_hint= {'center_x': 0.5,'center_y': 0.5},
-                )
-            )
-        )
+        layout= BoxLayout(orientation= 'vertical')
+        action= ActionBar()
+        layout.add_widget(action)
+        label= MDLabel(text="Lawis", halign= "center")
+        return layout
+        # self.theme_cls.theme_style = "Dark"
+        # self.theme_cls.primary_palette = "Red"
+    
+    # def on_start(self):
+    #     # self.fps_monitor_start()
+    #     pass
+        # return (
+        #     MDScreen(
+        #         MDRectangleFlatButton(
+        #             text= "Bonjour, Taysir.",
+        #             pos_hint= {'center_x': 0.5,'center_y': 0.5},
+        #         )
+        #     )
+        # )
         # return MDLabel(text="Bonjour, Taysir.", halign="center")
     
 
