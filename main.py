a4a051253f1cdf98ad0975ce9721d52da15a115d
subbu176 <ssarubn@gmail.com>
2024-01-06 17:26:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from kivymd.app import App
 from kivymd.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
 
+from kivy.config import Config
+Config.set('graphics', 'Resizeable', '0');
+Config.set('graphics', 'width', '412');
+Config.set('graphics', 'height', '732');
+
 class HomePage(Screen):
     pass
 
@@ -28,3 +33,8 @@ class ScreenManagement(ScreenManager):
 
 file = Builder.load_file("kivy.kv");
 
+class QuizApp(App):
+    def build(self):
+        return file
+    
+QuizApp().run()
